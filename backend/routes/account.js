52f4da399e2ee0c922888bd5c6c8285597127c44
Shubhanshu Arya <aryashubhanshu20@gmail.com>
2024-03-11 12:51:48 +0530

    Added sessions in transfer

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { authMiddleware } = require("../middleware");
 const { Account } = require("../db");
+const { default: mongoose } = require("mongoose");
 
 const router = express.Router();
 
@@ -15,13 +16,17 @@ router.get("/balance", authMiddleware, async (req, res) => {
 });
 
 router.post("/transfer", authMiddleware, async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
   const { amount, to } = req.body;
 
   const account = await Account.findOne({
     userId: req.userId,
-  });
+  }).session(session);
 
-  if (account.balance < amount) {
+  if (!amount || account.balance < amount) {
+    await session.abortTransaction();
     return res.status(400).json({
       message: "Insufficient funds",
     });
@@ -29,9 +34,10 @@ router.post("/transfer", authMiddleware, async (req, res) => {
 
   const toAccount = await Account.findOne({
     userId: to,
-  });
+  }).session(session);
 
   if (!toAccount) {
+    await session.abortTransaction();
     return res.status(400).json({
       message: "Account not found",
     });
@@ -46,7 +52,7 @@ router.post("/transfer", authMiddleware, async (req, res) => {
         balance: -amount,
       },
     }
-  );
+  ).session(session);
 
   await Account.updateOne(
     {
@@ -57,8 +63,9 @@ router.post("/transfer", authMiddleware, async (req, res) => {
         balance: amount,
       },
     }
-  );
+  ).session(session);
 
+  await session.commitTransaction();
   res.json({
     message: "Transaction successfully done",
   });
